4cfd84e26c09e91c21ee479ec5cbadcdce4a6398
Seyed Mostafa Kia <meganorm-smkia@ui-02.spider.surfsara.nl>
2024-05-01 17:49:49 +0200

    parallel utility function for job status is addeded

--- src/batch_job.sh
+++ src/batch_job.sh
@@ -2,11 +2,15 @@
 #SBATCH -N 1
 #SBATCH -c 1
 #SBATCH -p normal
+#SBATCH --mem=20GB
+#SBATCH --time=01:00:00
 #SBATCH -o /home/meganorm-smkia/temp/log/%j.out
+#SBATCH -e /home/meganorm-smkia/temp/log/%j.err
 
 source=$1
 target=$2
+subject=$3
 
 source activate mne 
 
-srun python /home/meganorm-smkia/Code/MEGaNorm/src/mainParallel.py $source $target
+srun --job-name=$subject python ./src/mainParallel.py $source $target
---
+++ utils/parallel.py
@@ -0,0 +1,47 @@
+import subprocess
+from datetime import datetime
+
+def check_user_jobs(username, start_time):
+    try:
+        # Format the current datetime to match Slurm's expected format
+        end_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+        
+        # sacct command to get job states and names within the specified time frame
+        cmd = ['sacct', '-n', '-X', '--parsable2', '--noheader',
+               '-S', start_time, '-E', end_time, '-u', username,
+               '--format=JobName,State']
+        
+        result = subprocess.run(cmd, capture_output=True, text=True)
+        
+        if result.returncode != 0:
+            print("Failed to query jobs:", result.stderr)
+            return
+        
+        # Initialize status counts and a list for failed job names
+        status_counts = {"PENDING": 0, "RUNNING": 0, "COMPLETED": 0, "FAILED": 0, "CANCELLED": 0}
+        failed_jobs = []
+        
+        # Process each line to count statuses and collect names of failed jobs
+        lines = result.stdout.strip().split('\n')
+        for line in lines:
+            if line:
+                parts = line.split('|')
+                if len(parts) >= 2:
+                    job_name, state = parts[0], parts[1]
+                    if state == 'PENDING':
+                        status_counts["PENDING"] += 1
+                    elif state == 'RUNNING':
+                        status_counts["RUNNING"] += 1
+                    elif state == 'COMPLETED':
+                        status_counts["COMPLETED"] += 1
+                    elif state == 'FAILED':
+                        status_counts["FAILED"] += 1
+                        failed_jobs.append(job_name)
+                    elif state == 'CANCELLED':
+                        status_counts["CANCELLED"] += 1
+
+        return status_counts, failed_jobs
+
+    except Exception as e:
+        print("An error occurred while checking the job status:", str(e))
+        return
