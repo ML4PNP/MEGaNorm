d6e0976afc656b0f0e46fe35d13d3fd473cfb670
Mohammad Zamanzadeh Nasrabadi <meganorm-mznasrabadi@ui-02.spider.surfsara.nl>
2024-08-05 10:34:55 +0200

    Gradiometer layout is added

--- layouts/layouts.py
+++ layouts/layouts.py
@@ -135,8 +135,32 @@ def create_layouts():
             "GRAD2_parietal_right": ["MEG1043", "MEG1113", "MEG1123", "MEG0723", "MEG1143", "MEG1133", "MEG0733", "MEG2213", "MEG2223", "MEG2243", "MEG2233", "MEG2443", "MEG2023"], 
             "GRAD2_occipital_left": ["MEG1643", "MEG1723", "MEG1713", "MEG1913", "MEG1943", "MEG1733", "MEG2043", "MEG1923", "MEG1933", "MEG1743", "MEG2113", "MEG2143"], 
             "GRAD2_occipital_right": ["MEG2433", "MEG2523", "MEG2533", "MEG2313", "MEG2323", "MEG2513", "MEG2033", "MEG2343", "MEG2333", "MEG2543", "MEG2123", "MEG2133"]
-        }
-                
+        },
+        "Megin_GRAD_All":{ 
+            'GRAD_All': ['MEG0122', 'MEG0342', 'MEG0312', 'MEG0322', 'MEG0512', 'MEG0542', 'MEG0332', 'MEG0522', 'MEG0532',
+                        'MEG0612', 'MEG0642', 'MEG0622', 'MEG0822', 'MEG1412', 'MEG1222', 'MEG1212', 'MEG1232', 'MEG0922', 
+                        'MEG0932', 'MEG1242', 'MEG0912', 'MEG0942', 'MEG1022', 'MEG1032', 'MEG0812', 'MEG1012', 'MEG0112', 
+                        'MEG0132', 'MEG0212', 'MEG0222', 'MEG0142', 'MEG1512', 'MEG0242', 'MEG0232', 'MEG1542', 'MEG1522', 
+                        'MEG1612', 'MEG1622', 'MEG1532', 'MEG1422', 'MEG1312', 'MEG1322', 'MEG1442', 'MEG1432', 'MEG1342', 
+                        'MEG1332', 'MEG2612', 'MEG2412', 'MEG2422', 'MEG2642', 'MEG2622', 'MEG2632', 'MEG0412', 'MEG0422', 
+                        'MEG0632', 'MEG0442', 'MEG0432', 'MEG0712', 'MEG1812', 'MEG1822', 'MEG0742', 'MEG1632', 'MEG1842', 
+                        'MEG1832', 'MEG2012', 'MEG1042', 'MEG1112', 'MEG1122', 'MEG0722', 'MEG1142', 'MEG1132', 'MEG0732', 
+                        'MEG2212', 'MEG2222', 'MEG2242', 'MEG2232', 'MEG2442', 'MEG2022', 'MEG1642', 'MEG1722', 'MEG1712', 
+                        'MEG1912', 'MEG1942', 'MEG1732', 'MEG2042', 'MEG1922', 'MEG1932', 'MEG1742', 'MEG2112', 'MEG2142', 
+                        'MEG2432', 'MEG2522', 'MEG2532', 'MEG2312', 'MEG2322', 'MEG2512', 'MEG2032', 'MEG2342', 'MEG2332', 
+                        'MEG2542', 'MEG2122', 'MEG2132', 'MEG0123', 'MEG0343', 'MEG0313', 'MEG0323', 'MEG0513', 'MEG0543', 
+                        'MEG0333', 'MEG0523', 'MEG0533', 'MEG0613', 'MEG0643', 'MEG0623', 'MEG0823', 'MEG1413', 'MEG1223', 
+                        'MEG1213', 'MEG1233', 'MEG0923', 'MEG0933', 'MEG1243', 'MEG0913', 'MEG0943', 'MEG1023', 'MEG1033', 
+                        'MEG0813', 'MEG1013', 'MEG0113', 'MEG0133', 'MEG0213', 'MEG0223', 'MEG0143', 'MEG1513', 'MEG0243', 
+                        'MEG0233', 'MEG1543', 'MEG1523', 'MEG1613', 'MEG1623', 'MEG1533', 'MEG1423', 'MEG1313', 'MEG1323', 
+                        'MEG1443', 'MEG1433', 'MEG1343', 'MEG1333', 'MEG2613', 'MEG2413', 'MEG2423', 'MEG2643', 'MEG2623', 
+                        'MEG2633', 'MEG0413', 'MEG0423', 'MEG0633', 'MEG0443', 'MEG0433', 'MEG0713', 'MEG1813', 'MEG1823', 
+                        'MEG0743', 'MEG1633', 'MEG1843', 'MEG1833', 'MEG2013', 'MEG1043', 'MEG1113', 'MEG1123', 'MEG0723', 
+                        'MEG1143', 'MEG1133', 'MEG0733', 'MEG2213', 'MEG2223', 'MEG2243', 'MEG2233', 'MEG2443', 'MEG2023', 
+                        'MEG1643', 'MEG1723', 'MEG1713', 'MEG1913', 'MEG1943', 'MEG1733', 'MEG2043', 'MEG1923', 'MEG1933', 
+                        'MEG1743', 'MEG2113', 'MEG2143', 'MEG2433', 'MEG2523', 'MEG2533', 'MEG2313', 'MEG2323', 'MEG2513', 
+                        'MEG2033', 'MEG2343', 'MEG2333', 'MEG2543', 'MEG2123', 'MEG2133']
+        }       
     }
     
     save_sensor_layouts(MEGIN_layouts, 'MEGIN')
