cc050e2c344a5ac5e61dfdc411336d67accf24ab
Mohammad Zamanzadeh Nasrabadi <meganorm-mznasrabadi@ui-01.spider.surfsara.nl>
2024-10-29 10:08:19 +0100

    make_demo_file_bids is added
    using this function we can convert any demographic data into a mne_bids compatible format file

--- datasets/mne_bids_conversion.py
+++ datasets/mne_bids_conversion.py
@@ -6,28 +6,31 @@ import mne
 from pathlib import Path
 import scipy
 from utils.EEGlab import read_raw_eeglab
+import pandas as pd
 
 
 def mne_bids_BTNRH(input_base_path,
                    output_path):
     
-
+    # make new data directory
     if os.path.exists(output_path): shutil.rmtree(output_path); os.mkdir(output_path)
 
-    raw_fpath = glob.glob(os.path.join(input_base_path, "*.fif"))
+    # since BTH dataset, does not have a demographic file in .tsv format, we
+    # create one for it so mne_bids can create the tsv file
+    BTNRH_covariates = pd.read_excel("/project/meganorm/Data/BTNRH/Rempe_Ott_PNAS_2022_Data.xlsx", index_col=0)
+    BTNRH_covariates.to_csv("participants.tsv", sep='\t', index=False)
 
+    raw_fpath = glob.glob(os.path.join(input_base_path, "*.fif"))
     for counter in range(len(raw_fpath)):
 
         subject_id = os.path.basename(raw_fpath[counter])[:7].split("-")[1]
-        
         raw = mne.io.read_raw_fif(raw_fpath[counter], verbose=False)
         raw.info["line_freq"] = 60
 
         bids_path = mne_bids.BIDSPath(
             task="rest",
             subject=subject_id,
-            root=output_path
-        )
+            root=output_path)
 
         # As stated in bellow link, apparantly CHPI signal must be droped. We have to investigate more later if we need other recordings
         # https://mne.discourse.group/t/chpi-channels-not-recognized-by-mne-bids-write-raw-bids/5609
@@ -36,9 +39,7 @@ def mne_bids_BTNRH(input_base_path,
         mne_bids.write_raw_bids(raw=raw,
                            bids_path=bids_path,
                            overwrite=True,
-                           verbose=False)
-        
-        
+                           verbose=False)      
     return None
 
 
@@ -148,6 +149,50 @@ def mne_bids_CMI(input_base_path, output_base_path, montage_path):
 
 
 
+def make_demo_file_bids(file_dir:str, save_dir:str, id_col:int, age_col:int, sex_col:int,
+                        male_indicator, female_indicator) -> None:
+
+    """
+    This function retrieves the address of a demographic file and converts 
+    it to a BIDS-compatible format. 
+    Ensure the output is saved in a directory structured according to BIDS specifications.
+
+    Parameters:
+        file_dir (str): Path to the input demographic file (e.g., CSV).
+        save_dir (str): Directory where the BIDS-formatted file should be saved.
+        id_col (int): Column index for the participant ID.
+        age_col (int): Column index for the age.
+        sex_col (int): Column index for the sex/gender.
+        male_indicator: Value in the sex column that indicates male.
+        female_indicator: Value in the sex column that indicates female.
+
+    Returns:
+        None
+    """
+    col_indices = {"participant_id" : id_col,
+                    "age" : age_col,
+                    "sex" : sex_col}
+    
+    if "xlsx" in file_dir[-4:]:
+        df = pd.read_excel(file_dir, index_col=None)
+    if "csv" in file_dir[-4:]:
+        df = pd.read_csv(file_dir, index_col=None)
+    if "tsv" in file_dir[-4:]:
+        df = pd.read_csv(file_dir, sep='\t', index_col=None)
+    
+
+    col_names = df.columns.to_list()
+    new_df = pd.DataFrame({})
+    # rearrange
+    for counter, (col_name, col_id) in enumerate(col_indices.items()): 
+        col = df[col_names[col_id]]
+        new_df.insert(counter, col_name, col)
+
+    new_df.dropna(inplace=True)
+    new_df.replace({"sex": {male_indicator:0, female_indicator:1}}, inplace=True)
+    new_df['age'] = new_df['age'].astype(int)
+
+    new_df.to_csv(save_dir, sep='\t', index=False)
 
 
 
@@ -170,3 +215,25 @@ if __name__ == "__main__":
     mne_bids_CMI(input_base_path, output_base_path, montage_path)
 
 
+
+    # Preparing demographic data
+    # BTH
+    file_dir = "/project/meganorm/Data/BTNRH/Rempe_Ott_PNAS_2022_Data.xlsx"
+    save_dir = "/project/meganorm/Data/BTNRH/BTNRH/BIDS_data/participants.tsv"
+    make_demo_file_bids(file_dir, 
+                        save_dir, id_col=0, 
+                        age_col=1, 
+                        sex_col=2, 
+                        male_indicator="M", 
+                        female_indicator="F")
+
+    # CAMCAN
+    file_dir = "/project/meganorm/Data/camcan/CamCAN/cc700/participants.tsv"
+    save_dir = "/project/meganorm/Data/BTNRH/CAMCAN/BIDS_data/participants.tsv"
+    make_demo_file_bids(file_dir, 
+                        save_dir, 
+                        id_col=0, 
+                        age_col=1, 
+                        sex_col=3, 
+                        male_indicator="MALE", 
+                        female_indicator="FEMALE")
