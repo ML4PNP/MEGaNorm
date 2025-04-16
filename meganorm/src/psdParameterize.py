3f52185337b4ee739daad36ecdb4317b5db3c916
Mohammad Zamanzadeh <m.zamanzadeh.98@gmail.com>
2025-04-16 15:03:25 +0200

    doc string is added to src/psdParameterize.py

--- meganorm/src/psdParameterize.py
+++ meganorm/src/psdParameterize.py
@@ -7,67 +7,69 @@ import argparse
 from tqdm import tqdm
 from glob import glob
 import fooof as f
-
-#parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#config_path = os.path.join(parent_dir, 'utils')
-#sys.path.append(config_path)
 from meganorm.utils.IO import make_config, storeFooofModels
 
 import warnings
 warnings.filterwarnings('ignore')
 
 
-
-
-
-def computePsd(segments, freqRangeLow=3, freqRangeHigh=40, sampling_rate=1000, psdMethod="welch", psd_n_overlap=1, psd_n_fft=2, n_per_seg=2):
+def computePsd(segments, freqRangeLow=3, freqRangeHigh=40, sampling_rate=1000, psdMethod="welch", 
+               psd_n_overlap=1, psd_n_fft=2, n_per_seg=2):
     """
-    compute power spectrum
-    """
+    Computes the Power Spectral Density (PSD) for the given segments of EEG/MEG data.
 
+    Args:
+        segments (mne.Epochs): Segmented data for which PSD will be computed.
+        freqRangeLow (int): Lower frequency bound for PSD calculation (Hz).
+        freqRangeHigh (int): Upper frequency bound for PSD calculation (Hz).
+        sampling_rate (int): The sampling rate of the data (Hz).
+        psdMethod (str): Method for computing the PSD. Default is "welch".
+        psd_n_overlap (int): The overlap between segments (in seconds) for PSD calculation.
+        psd_n_fft (int): The number of FFT points used for the PSD calculation.
+        n_per_seg (int): The number of samples per segment used for computing PSD.
+
+    Returns:
+        psds (np.ndarray): Array of power spectral density values.
+        freqs (np.ndarray): Array of frequency values corresponding to the PSD.
+    """
+    
     psds, freqs = segments.compute_psd( 
-                    method = psdMethod,
-                    fmin = freqRangeLow,
-                    fmax = freqRangeHigh,
-                    n_jobs = -1,
-                    average = "mean",
-                    # having at least two cycles of 1Hz activity
-                    n_overlap = psd_n_overlap*sampling_rate, 
-                    n_fft = psd_n_fft*sampling_rate, 
-                    n_per_seg = n_per_seg*sampling_rate, 
+                    method=psdMethod,
+                    fmin=freqRangeLow,
+                    fmax=freqRangeHigh,
+                    n_jobs=-1,
+                    average="mean",
+                    n_overlap=psd_n_overlap * sampling_rate, 
+                    n_fft=psd_n_fft * sampling_rate, 
+                    n_per_seg=n_per_seg * sampling_rate, 
                     verbose=False).average().get_data(return_freqs=True)
     
     return psds, freqs
-    
+
 
 def parameterizePsd(psds, freqs, freqRangeLow=3, freqRangeHigh=40, min_peak_height=0,
                     peak_threshold=2, peak_width_limits=[1, 12.0], aperiodic_mode="fixed"):
-
     """
-    fooofModeling fit multiple models (group fooof) and returns 
-    periodic and aperiodic signals
+    Fits a FOOOF model to the power spectral density (PSD) data to decompose 
+    the signal into periodic and aperiodic components.
 
-    parameters
-    --------------
-    segments: ndarray
-    segmented data
+    Args:
+        psds (np.ndarray): Power spectral density values.
+        freqs (np.ndarray): Frequency values corresponding to the PSD.
+        freqRangeLow (int): Lower frequency bound for the FOOOF model (Hz).
+        freqRangeHigh (int): Upper frequency bound for the FOOOF model (Hz).
+        min_peak_height (float): Minimum height of peaks to be considered in the FOOOF model.
+        peak_threshold (float): Threshold for peak detection in the FOOOF model.
+        peak_width_limits (list): Limits on the width of peaks (in Hz).
+        aperiodic_mode (str): Mode to model the aperiodic component, options are "fixed", "knee", or "none".
 
-    spectrumMethod: str
-    the method to be used to calculate power spectrum
-
-    fs:int
-    sampling rate
+    Returns:
+        fooofModels (FOOOFGroup): Fitted FOOOF group model containing periodic and aperiodic components.
+        psds (np.ndarray): Original power spectral density values.
+        freqs (np.ndarray): Frequency values corresponding to the PSD.
+    """
     
-    freqRange: list[int]
-    desired frequency range to be modeled by fooof model
-    default: [1, 60]
-
-    return
-    --------------
-    fooofModels: object
-    """  
-
-    # fitting seperate models for each channel
+    # Fit separate models for each channel
     fooofModels = f.FOOOFGroup(peak_width_limits=peak_width_limits, 
                                 min_peak_height=min_peak_height, 
                                 peak_threshold=peak_threshold, 
@@ -78,41 +80,41 @@ def parameterizePsd(psds, freqs, freqRangeLow=3, freqRangeHigh=40, min_peak_heig
 
 
 def psdParameterize(segments, freqRangeLow=3, freqRangeHigh=40, min_peak_height=0,
-        peak_threshold=2, sampling_rate=1000, psdMethod="welch", psd_n_overlap=1, 
-        psd_n_fft=2, n_per_seg=2, peak_width_limits=[1, 12.0], aperiodic_mode="fixed") -> None: 
+                    peak_threshold=2, sampling_rate=1000, psd_method="welch", psd_n_overlap=1, 
+                    psd_n_fft=2, n_per_seg=2, peak_width_limits=[1, 12.0], aperiodic_mode="knee"):
     """
-    following steps are included in this function
-    1. data segmentation
-    2. croping data (using tmin and tmax)
-    3. interpolate bad segments
-    4. calculate power spectrum
-    5. apply fooof algorithm to parametrize neural spectrum
+    Runs the complete pipeline for spectral parameterization using FOOOF. 
+    This includes computing the PSD and fitting FOOOF models for each segment/channel.
 
-    parameters
-    -----------
-    dataPaths: str
-    data address
+    Args:
+        segments (mne.Epochs): Epoched MNE object containing segmented data.
+        freqRangeLow (float): Lower bound of frequency range for PSD and FOOOF (Hz).
+        freqRangeHigh (float): Upper bound of frequency range for PSD and FOOOF (Hz).
+        min_peak_height (float): Minimum height of peaks to be detected by FOOOF.
+        peak_threshold (float): Threshold for peak detection relative to aperiodic fit.
+        sampling_rate (int): Sampling frequency of the signal (Hz).
+        psd_method (str): Method used to compute PSD. Options: "welch", "multitaper".
+        psd_n_overlap (int): Overlap (in seconds) between segments in PSD computation.
+        psd_n_fft (int): Number of FFT points (in seconds) used in PSD.
+        n_per_seg (int): Length (in seconds) of each segment used in PSD.
+        peak_width_limits (list): Lower and upper bounds on peak width (Hz).
+        aperiodic_mode (str): Mode of aperiodic fit. Options: "fixed" or "knee".
 
-    save_path: str
-    where to save data
-
-    freqRangeLow: float
-    Desired frequency range to run FOOOF
-
-    freqRangeHigh: float
-    Desired frequency range to run FOOOF
-
-
-    returns
-    ------------
-    None
+    Returns:
+        fooofModels (FOOOFGroup): Fitted FOOOF models for each channel.
+        psds (ndarray): Power spectral densities.
+        freqs (ndarray): Corresponding frequency values.
     """
-
+    if psd_method not in ["multitaper", "welch"]:
+        raise ValueError("psd_method must be either 'welch' or 'multitaper'")
+    if aperiodic_mode not in ["fixed", "knee"]:
+        raise ValueError("aperiodic_mode must be either 'knee' or 'fixed'")
+        
     psds, freqs = computePsd(segments=segments, 
                             freqRangeLow=freqRangeLow, 
                             freqRangeHigh=freqRangeHigh, 
                             sampling_rate=sampling_rate, 
-                            psdMethod=psdMethod, 
+                            psd_method=psd_method, 
                             psd_n_overlap=psd_n_overlap, 
                             psd_n_fft=psd_n_fft,
                             n_per_seg=n_per_seg)
@@ -128,16 +130,7 @@ def psdParameterize(segments, freqRangeLow=3, freqRangeHigh=40, min_peak_height=
                                             aperiodic_mode=aperiodic_mode)
 
     return fooofModels, psds, freqs
-
-
-            
-
         
-
-
-
-        
-
 
 if __name__ == "__main__":
 
@@ -150,13 +143,11 @@ if __name__ == "__main__":
         help="Address of configs json file")
     args = parser.parse_args()
 
-
     # Loading configs
     if args.configs is not None:
         with open(args.configs, 'r') as f:
             configs = json.load(f)
     else: configs = make_config()
-
 
     dataPaths = glob(args.dir)
 
@@ -168,7 +159,6 @@ if __name__ == "__main__":
         data = mne.io.read_raw_fif(subjPath,
                                 preload=True,
                                 verbose=False)
-
 
         # parametrizing neural spectrum and saving the res
         fooofModels, psds, freqs = psdParameterize(data = data,
@@ -186,7 +176,6 @@ if __name__ == "__main__":
                                         psd_n_fft = configs["psd_n_fft"],
                                         n_per_seg = configs["n_per_seg"],
                                         peak_width_limits = configs["peak_width_limits"])
-        
 
         # save the results
         storeFooofModels(configs["savePath"], 
