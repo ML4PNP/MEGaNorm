cc9c246ae59f1cf5ce94bd49cc0b2ac08e9a0acb
Ymke Verduyn <meganorm-yverduyn@ui-02.spider.surfsara.nl>
2025-04-17 15:55:28 +0200

    cleaned TDBrain dataset file

--- meganorm/datasets/TDBrain.py
+++ meganorm/datasets/TDBrain.py
@@ -5,12 +5,30 @@ import pandas as pd
 
 def define_eog_ecg_emg_channels_TDBrain(input_base_path):
 
+    """Update channel types for TDBrain EEG channel TSV files (only for restEC task, session 1)
+
+    This function scans the EEG channel metadata files for the TDBrain dataset
+    and updates specific channels to appropriate types: "EOG", "ECG", or "EMG".
+    The updated TSV files are saved in-place.
+
+    Parameters
+    ----------
+    input_base_path : _type_
+        Path to the root directory of the TDBrain dataset. It should contain
+        subject/session folders with EEG data organized under `sub-*/ses-1/eeg/`.
+    
+    Returns
+    -------
+    None
+        This function performs file operations and does not return any value.
+        The modified files are saved directly to disk.
+    """
+
     search_pattern = os.path.join(
         input_base_path, "*/ses-1/eeg/*_ses-1_task-restEC_channels.tsv"
     )
     channel_files = glob.glob(search_pattern)
-    print("found:", channel_files)
-
+    
     for file in channel_files:
         channels_df = pd.read_csv(file, sep="\t")
         # Define the mappings for channel types
@@ -36,8 +54,3 @@ def define_eog_ecg_emg_channels_TDBrain(input_base_path):
         channels_df.to_csv(file, sep="\t", index=False)
 
         print("Channel types updated successfully.")
-
-
-if __name__ == "__main__":
-    path = "/project/meganorm/Data/EEG_TDBrain/EEG/"
-    define_eog_ecg_emg_channels_TDBrain(path)
