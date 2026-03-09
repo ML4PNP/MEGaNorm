7d3fc85d04e4a023536f8e2adf7decbced7e75c6
Mohammad Zamanzadeh <m.zamanzadeh.98@gmail.com>
2026-03-09 14:29:25 +0100

    Minor

--- meganorm/utils/IO.py
+++ meganorm/utils/IO.py
@@ -1003,10 +1003,12 @@ def set_path(project_dir):
     make_folder(exluded_participants_path)
 
     # Normative models
-    nm_dir = os.path.join(project_dir, "Normative_modeling")
+    nm_dir = os.path.join(project_dir, "Normative_models")
     run_dir = os.path.join(nm_dir, "Runs")
     figures_dir = os.path.join(nm_dir, "Figures")
-    models_summary = os.path.join(nm_dir, "Models summary")
+    models_summary = os.path.join(nm_dir, "Models_summary")
+    nm_parallel_logs = os.path.join(nm_dir, "nm_parallel_logs")
+    nm_temp = os.path.join(nm_dir, "nm_temp")
 
     make_folder(nm_dir)
     make_folder(run_dir)
