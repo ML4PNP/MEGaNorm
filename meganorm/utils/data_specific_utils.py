38082ad7dd8e1af208ec3af44d9e7782a10a4d31
Mohammad Zamanzadeh <m.zamanzadeh.98@gmail.com>
2026-07-16 11:13:18 +0200

    a function necessary for corregisteration of NIMH

--- meganorm/utils/data_specific_utils.py
+++ meganorm/utils/data_specific_utils.py
@@ -1,6 +1,10 @@
 import mne
 import numpy as np
-
+import json
+import numpy as np
+import nibabel as nib
+import mne
+from mne.io.constants import FIFF
 
 def _ast_get_rs_events(raw, start_channel='STI001', end_channel='STI002'):
     """Returns paired (start_sample, end_sample) for each resting block."""
@@ -21,3 +25,47 @@ def _ast_get_rs_block(raw, block_index, start_channel='STI001', end_channel='STI
     tmax = (end_samp - raw.first_samp) / raw.info['sfreq']
 
     return raw.copy().crop(tmin=tmin, tmax=tmax)
+
+
+
+def _trans_from_nimh(raw, coordsystem_json, subject, subjects_dir):
+
+    cs = json.load(open(coordsystem_json))
+    alm = cs["AnatomicalLandmarkCoordinates"]
+
+    # The MRI landmarks are in scanner space, but MNE works in FreeSurfer surface RAS space; a different origin
+    t1 = nib.load(f"{subjects_dir}/{subject}/mri/T1.mgz")
+    scanner_to_surf = t1.header.get_vox2ras_tkr() @ np.linalg.inv(
+        t1.header.get_vox2ras()
+    )
+
+    def lps_mm_to_surf_m(p):
+        p_ras = np.array(p, float) * [-1, -1, 1]
+        return (scanner_to_surf @ np.r_[p_ras, 1.0])[:3] / 1000.0
+
+    fids = [
+        dict(
+            kind=FIFF.FIFFV_POINT_CARDINAL,
+            ident=FIFF.FIFFV_POINT_LPA,
+            r=lps_mm_to_surf_m(alm["LPA"]),
+            coord_frame=FIFF.FIFFV_COORD_MRI,
+        ),
+        dict(
+            kind=FIFF.FIFFV_POINT_CARDINAL,
+            ident=FIFF.FIFFV_POINT_NASION,
+            r=lps_mm_to_surf_m(alm["NAS"]),
+            coord_frame=FIFF.FIFFV_COORD_MRI,
+        ),
+        dict(
+            kind=FIFF.FIFFV_POINT_CARDINAL,
+            ident=FIFF.FIFFV_POINT_RPA,
+            r=lps_mm_to_surf_m(alm["RPA"]),
+            coord_frame=FIFF.FIFFV_COORD_MRI,
+        ),
+    ]
+
+    coreg = mne.coreg.Coregistration(
+        raw.info, subject=subject, subjects_dir=subjects_dir, fiducials=fids
+    )  # list, not a filename
+    coreg.fit_fiducials()
+    return coreg, fids
