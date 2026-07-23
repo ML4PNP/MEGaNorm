b51b90e34948b191ace38e10eced15f6887a5205
Mohammad Zamanzadeh <m.zamanzadeh.98@gmail.com>
2026-07-23 14:40:20 +0200

    if BTI and hs_file not present, space is kept in BTI space

--- meganorm/utils/IO.py
+++ meganorm/utils/IO.py
@@ -657,17 +657,23 @@ def load_recording(device, path, empty_room_recording_path, configs, logger):
     elif device == "BTI":
         temp_hs_file_path = os.path.join(path, "hs_file")
         hs_file = temp_hs_file_path if os.path.exists(temp_hs_file_path) else None
+        if not hs_file:
+            convert = False
+        else:
+            convert = True
         data = mne.io.read_raw_bti(
             pdf_fname=os.path.join(path, "c,rfDC"),
             config_fname=os.path.join(path, "config"),
             head_shape_fname=hs_file,
             preload=True,
+            convert=convert
         )
         if empty_room_recording_path and configs.apply_source_localization:
             empty_room_recording = mne.io.read_raw_bti(
                 pdf_fname=os.path.join(empty_room_recording_path, "c,rfDC"),
                 config_fname=os.path.join(empty_room_recording_path, "config"),
                 head_shape_fname=None,
+                convert=convert,
                 preload=True,
             )
             logger.info("Empty room recording was found")
